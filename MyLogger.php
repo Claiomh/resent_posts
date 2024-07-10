d36df14e01593a9475218ef1680e651f086c83e2
Claiomh <37008340+Claiomh@users.noreply.github.com>
2024-07-10 20:03:14 +0500

    Create MyLogger.php

---
+++ MyLogger.php
@@ -0,0 +1,56 @@
+<?php
+
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+class MyLogger implements LoggerInterface
+{
+    public function emergency($message, array $context = [])
+    {
+        $this->log(LogLevel::EMERGENCY, $message, $context);
+    }
+
+    public function alert($message, array $context = [])
+    {
+        $this->log(LogLevel::ALERT, $message, $context);
+    }
+
+    public function critical($message, array $context = [])
+    {
+        $this->log(LogLevel::CRITICAL, $message, $context);
+    }
+
+    public function error($message, array $context = [])
+    {
+        $this->log(LogLevel::ERROR, $message, $context);
+    }
+
+    public function warning($message, array $context = [])
+    {
+        $this->log(LogLevel::WARNING, $message, $context);
+    }
+
+    public function notice($message, array $context = [])
+    {
+        $this->log(LogLevel::NOTICE, $message, $context);
+    }
+
+    public function info($message, array $context = [])
+    {
+        $this->log(LogLevel::INFO, $message, $context);
+    }
+
+    public function debug($message, array $context = [])
+    {
+        $this->log(LogLevel::DEBUG, $message, $context);
+    }
+
+    public function log($level, $message, array $context = [])
+    {
+        $logMessage = strtoupper($level) . ': ' . $message;
+        if (!empty($context)) {
+            $logMessage .= ' ' . json_encode($context);
+        }
+        error_log($logMessage);
+    }
+}
